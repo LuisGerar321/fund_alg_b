f071624fb673cd3b60cee7d371988288f1313db4
LuisGerar321 <dojas64@gmail.com>
2022-02-02 11:06:28 -0600

    local variables internal main

---
+++ unit1/variablesGlobalLocal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//*Global Variables*//
+char* date = "02-02-22T10:25:00z";
+///////////////////////////////
+
+/// La ejecucion del programa///////
+int main(){
+    date = "02-03-22T10:25:00z";
+    int hours = 22; //Local:main 
+    hours = hours + 2;
+    printf("hours: %d\n", hours);
+    return 0;
+}
+///////////////////////////////
